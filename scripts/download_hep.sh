45212186f87d1ca33a551cd66aba99f3bcec4a75
ambertrujillo <45443896+ambertrujillo@users.noreply.github.com>
2020-09-22 15:09:55 -0400

    Update and rename download_vivax.sh to download_hep.sh

--- scripts/download_hep.sh
+++ scripts/download_hep.sh
@@ -1,21 +1,21 @@
 #!/bin/sh
 
-# Script to download Plasmodium vivax genome, Pvivax
+# Script to download Plasmodium gonderi genome, gonderi
 
 module load kent/328
 
-mkdir -p genomes
-cd genomes
+mkdir -p genomes/
+cd genomes/
 
-GENOME_FA=vivax.fa
+GENOME_FA=hep.fa
 
-PVIVAX_URL=ftp://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/002/415
-PVIVAX_URL=$PVIVAX_URL/GCF_000002415.2_ASM241v2/GCF_000002415.2_ASM241v2_genomic.fna.gz
+HEPATOCYSTIS_URL=ftp://ftp.ncbi.nlm.nih.gov/genomes/all/GCA/902/459/845
+HEPATOCYSTIS_URL=$HEPATOCYSTIS_URL/GCA_902459845.2_HEP1/GCA_902459845.2_HEP1_genomic.fna.gz 
 
-wget $PVIVAX_URL \
+wget $HEPATOCYSTIS_URL \
     -O ${GENOME_FA}.gz
 gunzip -c ${GENOME_FA}.gz | \
-    sed -e "s/^>.*chromosome \([^,]*\),.*/>Plaschr\\1/" > \
+    sed -e "s/^>.*chromosome \([^,]*\),.*/>Hepchr\\1/" > \
     $GENOME_FA
 
 echo "Getting rid of unassembled stuff..." >&2
@@ -24,7 +24,7 @@ LAST_OK_LINE=$((`grep -n "^>[^c]" $GENOME_FA | head -n 1 | cut -d":" -f 1` - 1))
 if [ $LAST_OK_LINE -gt 0 ]; then
     mv $GENOME_FA ${GENOME_FA}.backup
     head -n $LAST_OK_LINE ${GENOME_FA}.backup > ${GENOME_FA}
-    rm ${GENOME_FA}.backup
+    rm ${GENOME_FA}.backup   
 fi
 
 mkdir tmp_for_sort
